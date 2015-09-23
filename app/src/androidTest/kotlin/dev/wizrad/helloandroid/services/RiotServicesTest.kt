0056d16daeeeb465c164cd00c28971d419464651
Ty Cobb <ty.cobb.m@gmail.com>
2015-09-23 11:47:27 -0500

    added kotlin tests directory

---
+++ app/src/androidTest/kotlin/dev/wizrad/helloandroid/services/RiotServicesTest.kt
@@ -0,0 +1,12 @@
+package dev.wizrad.helloandroid.services
+
+import android.test.AndroidTestCase
+import kotlin.test.assertEquals
+
+public class RiotServicesTest : AndroidTestCase() {
+
+    fun testServices() {
+        assertEquals(false, true);
+    }
+
+}
