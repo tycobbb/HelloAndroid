6829f1821d68f2333e7d3b6a0bda07f1118387a3
Ty Cobb <ty.cobb.m@gmail.com>
2015-09-30 02:16:44 -0500

    added support.design and theme

--- app/src/androidTest/kotlin/dev/wizrad/helloandroid/services/RiotServicesTest.kt
+++ app/src/androidTest/kotlin/dev/wizrad/helloandroid/services/RiotServicesTest.kt
@@ -1,12 +1,25 @@
 package dev.wizrad.helloandroid.services
 
-import android.test.AndroidTestCase
+import dev.wizrad.helloandroid.BuildConfig
 import kotlin.test.assertEquals
+import android.test.AndroidTestCase
+import dev.wizrad.helloandroid.services.utilities.LoggingInterceptor
+import kotlin.test.assertNotNull
 
 public class RiotServicesTest : AndroidTestCase() {
 
-    fun testServices() {
-        assertEquals(false, true);
+    fun test_hasAnApiKey() {
+        assertEquals(RiotServices.API_KEY, BuildConfig.RIOT_API_KEY)
+    }
+
+    fun test_createsLeagueService() {
+        val service = RiotServices.create(LeagueService::class.java)
+        assertNotNull(service, "failed to instantiate a LeagueService")
+    }
+
+    fun test_createsSummonerService() {
+        val service = RiotServices.create(SummonerService::class.java)
+        assertNotNull(service, "failed to instantiate a SummonerService")
     }
 
 }
